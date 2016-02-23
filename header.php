3ebb4ed4d684433ce9cb124e60befcdb1780d6a9
Rob Berger <rob.a.berger@gmail.com>
2016-02-23 14:24:28 -0500

    Add logo

--- header.php
+++ header.php
@@ -43,7 +43,7 @@
 		<nav id="site-navigation" class="main-navigation top-bar" role="navigation">
 			<div class="top-bar-left">
 				<ul class="menu">
-					<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
+					<li class="home"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/DRLogo192.png"></a></li>
 				</ul>
 			</div>
 			<div class="top-bar-right">
